52e80be6f3e97f833830ee521866f0879a9de406
MatteoCarbone17 <matteocarbone17@live.com>
2022-12-01 16:19:07 +0100

    array container + random number

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-/*Consegna:
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+/*Consegna
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco
+(attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+
+Attenzione: Nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+
+Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
 */
 
 
@@ -12,29 +23,7 @@ const gridContainer = document.querySelector('section.grid');
 
 const button = document.querySelector('a.btn-primary');
 
-button.addEventListener('click', function () {
-
-
-    gridContainer.innerHTML='';
-   
-    for (let i = 1; i < 100; i++) {
-
-        const newSquare = getElementNewSquare(); 
-
-
-        newSquare.innerHTML = '<span class="m-auto">'  + i + '</span>';
-        
-        gridContainer.appendChild(newSquare);  
-    }
-    
-
-
-});
-
-
-
-
-//funzione che crea il quadrato
+const container= []; 
 
 function getElementNewSquare() {
     
@@ -50,3 +39,45 @@ function getElementNewSquare() {
     return newSquare;
 
 }
+
+
+for (let i = 1; i < 100 ; i++){
+    
+    randomIndex = getRanndomNumber( 1 , 100);
+
+    container.push(randomIndex);
+
+}
+
+ console.log(container)
+
+function getRanndomNumber(numMin,numMax) {
+
+  return Math.floor(Math.random() * (numMax - numMin + 1) + numMin );
+
+}
+ 
+
+button.addEventListener('click', function () {
+
+
+    gridContainer.innerHTML='';
+   
+    for (let i = 1; i < 100; i++) {
+
+        const newSquare = getElementNewSquare(); 
+
+        newSquare.innerHTML = '<span class="m-auto">'  + i + '</span>';
+        
+        gridContainer.appendChild(newSquare);  
+    }
+    
+
+
+});
+
+
+
+
+//funzione che crea il quadrato
+
